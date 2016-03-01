aef4e8d876e7427b64133e17e0230ea4c137090a
ymachi2 <ymachi2@uic.edu>
2016-02-29 23:48:47 -0600

    Updated GameTest.Java with topTen tests

--- GameTest.java
+++ GameTest.java
@@ -1,39 +1,88 @@
 import static org.junit.Assert.*;
- 
-import org.junit.Test;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.util.Scanner;
 import java.io.*;
+import org.junit.Test;
  
 public class GameTest {
  
     MineSweeperGrid game = new MineSweeperGrid();
-    main main = new main();
-    Icon smiley = new ImageIcon("CS342 Project 2 Minesweeper Images/smile_button.gif");
-    JButton testSmileyButton; 
-    MineSweeperGrid.MSButton msButton = game.new MSButton(0,0,"",smiley);
-    boolean buttonMineStatus;
-    int x;
+    File fileName = new File("topTen.txt");
+    String firstString;
     
- 
+    
+    @Test
+    public void testTopTenFile(){
+      game.resetTopTen();
+      
+      assertNotNull("Check topTen.txt is not null", fileName);
+      assertEquals("Checking default first line of topTen.txt", "1. COMP1 999", readFile());
+      assertEquals("Checking there are 10 lines in topTen.txt", 10, getFileLineCount());
+      
+      game.insertTopTen(90, "Yordan");
+      
+      assertEquals("Checking first line of topTen.txt after insert", "1. Yordan 90", readFile());
+      game.resetTopTen();
+      assertEquals("Checking default first line of topTen.txt after a reset", "1. COMP1 999", readFile());
+      
+    }
+    
     @Test
     public void testCheckTopTen() {
-       assertTrue("Checking topTen", game.checkTopTen(90));
-       assertFalse("Checking topTen", game.checkTopTen(1000));
-        //assertFalse("This should fail", game.checkTopTen(90));  
+      assertTrue("Checking topTen", game.checkTopTen(90));
+      assertFalse("Checking topTen for value over true threshold.", game.checkTopTen(1000));
+      assertTrue("Checking topTen for lowest possible value of true threshold", game.checkTopTen(0));
+      //assertFalse("This should fail", game.checkTopTen(90));  
     }
     
-    @Test
-    public void testInitialMineStatus() {
-      buttonMineStatus = msButton.mineStatus;
-      assertFalse("Checking initial mine status",buttonMineStatus);
+    public String readFile(){
+      try{
+        Scanner input = new Scanner(fileName);
+      
+        int i = 1;
+        while(input.hasNextLine())
+        {
+          if(i == 1){
+            firstString = input.nextLine();
+            ++i;
+          }
+          else{
+            String progress = input.nextLine();
+            ++i;
+          }
+        }
+        input.close();
+      }
+      catch(Exception ex){
+        System.out.println("File not Found");
+      }
+      
+      return firstString;
     }
     
-    @Test
-    public void testToggledMineStatus() {
-      buttonMineStatus = false;
-      msButton.toggleMine(buttonMineStatus);
-      assertFalse("Checking toggled mine status",buttonMineStatus);
+    public int getFileLineCount(){
+      int i = 1;
+      
+      try{
+        Scanner input = new Scanner(fileName);
+      
+        i = 1;
+        while(input.hasNextLine())
+        {
+          if(i == 1){
+            firstString = input.nextLine();
+            ++i;
+          }
+          else{
+            String progress = input.nextLine();
+            ++i;
+          }
+        }
+        input.close();
+      }
+      catch(Exception ex){
+        System.out.println("File not Found");
+      }
+      
+      return i-1; //This should be 10
     }
 }
